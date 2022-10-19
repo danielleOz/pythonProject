a09f98daf0902401c79174431bca03dc9c1a312d
danielleOz <danielle.oz.do@gmail.com>
2022-10-19 16:52:02 +0300

    add adress

---
+++ ExmpleGit (1).py
@@ -0,0 +1,14 @@
+#1
+print(" Hello Enter your  name: ")
+print("danielle")
+#2
+print(" Hello Enter your adress: ")
+print("hod hasharon")
+
+
+"""
+name = "Name" 
+print(" Hello " , name ," How are you: " )
+"""
+
+
