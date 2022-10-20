72010fc6ff08fde5e7a02f975743b000c5ab3f01
danielleOz <danielle.oz.do@gmail.com>
2022-10-20 09:59:47 +0300

    add animal

--- ExmpleGit (1).py
+++ ExmpleGit (1).py
@@ -1,10 +1,7 @@
 #1
 print("cat")
 print("danielle")
-<<<<<<< HEAD
 print("hi")
-=======
->>>>>>> abfbe64d075e8edc877cb3a3443722ed3f6a0d3d
 #2
 print(" Hello Enter your adress: ")
 print("hod hasharon")
